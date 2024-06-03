e80727f011e2f05eafe54b593c4842f3c4cd73c7
fredd <fredd@QJM-FREDDY>
2024-06-03 08:08:47 -0400

    Add project files.

---
+++ AppSample/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSample.ViewModel
+{
+    public partial class MainViewModel : ObservableRecipient
+    {
+        [ObservableProperty]
+        public ObservableObject currentViewModel = new MenuViewModel();
+    }
+}
